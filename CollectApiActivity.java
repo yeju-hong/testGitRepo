5f2d852cffbaaf56f73bca49c386c478204f0246
yeju-hong <73091760+yeju-hong@users.noreply.github.com>
2024-12-03 09:57:06 +0900

    Create CollectApiActivity.java
    first main commit

---
+++ CollectApiActivity.java
@@ -0,0 +1,13 @@
+package com.inissoft.collectin.activity;
+
+import java.util.Map;
+
+import org.apache.commons.collections4.MapUtils;
+
+public class CollectApiActivity extends DefaultActivity {
+
+	public void execute(CollectInContext context, Map<String, Object> p) {
+
+	}
+
+}
